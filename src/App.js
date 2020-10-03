d1fcdc9ace71e4673d0512d4c0a39656f1bba27f
Manuel Ortega Carcamo <mortega@apgca.cl>
2020-10-03 01:35:17 -0300

    chore: initialize repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+
+const App =()=> {
+  
+  // Add Task
+  const handleFormSubmit = async (e) =>  {
+      console.log('--e--', e);
+  };
+
+  // Remove Task
+  const deleteTask = (taskId) => 
+  {
+    console.log('--taskId--', taskId);
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>To Do List</h1>
+        <section className="task-list">
+          <form onSubmit={ (e)=> handleFormSubmit(e) }>
+            <input
+              autoFocus={true}
+              className="new-todo"
+              placeholder="Que tienes que hacer?"
+              onChange={(e) => console.log(e.target.value) }
+            />
+          </form>
+          <section className="main">
+                <ul className="todo-list">
+                {
+                  (<span>No hay tareas</span>)
+                }
+
+                  <li key={i}>
+                    <span className='with90'>{item.task}</span>  
+                    <a className='with10' onClick={ ()=> deleteTask(i)}>X</a>
+                  </li>
+                </ul>
+          </section>
+          <footer className="footer">
+                0 items
+          </footer>
+
+        </section>
+      </div>
+    </>
   );
 }
 
